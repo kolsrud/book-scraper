2cfd637b3a55780a29c9e7a4cc1b933d4c0da71d
Øystein Kolsrud <yko@qlik.com>
2024-01-14 14:22:24 +0100

    Initial project.

---
+++ BookScraper/Program.cs
@@ -0,0 +1,15 @@
+﻿using HtmlAgilityPack;
+
+namespace BookScraper
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var url = "https://books.toscrape.com/";
+			var web = new HtmlWeb();
+			var html = web.LoadFromWebAsync(url).Result;
+			Console.WriteLine(html.DocumentNode.WriteContentTo());
+		}
+	}
+}
